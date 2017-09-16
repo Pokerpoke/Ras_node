3bb582a05211d5ddbc27f488b00a03e5f55bbc54
Jiang <j824544269@gmail.com>
2017-09-16 15:22:53 +0800

    Feat: - can capture voice now

--- an-core/lib/voice_capture.cc
+++ an-core/lib/voice_capture.cc
@@ -1,1 +1,178 @@
+#include "voice_capture.h"
+#include "logger.h"
 #include <iostream>
+
+namespace an
+{
+namespace core
+{
+
+voice_capture::voice_capture()
+{
+	device = new char[20];
+	strcpy(device, "default");
+
+	format = SND_PCM_FORMAT_S16_LE;
+	channels = 2;
+	access_type = SND_PCM_ACCESS_RW_INTERLEAVED;
+	rate = 44100;
+	frames_to_read = 32;
+
+	DEVICE_OPENED = false;
+	PARAMS_SETED = false;
+	output_buffer_size = frames_to_read * snd_pcm_format_width(format) / 8 * 2;
+
+	output_buffer = (char *)malloc(output_buffer_size);
+}
+
+voice_capture::voice_capture(const char *t_device)
+{
+	device = new char[20];
+	strcpy(device, "default");
+
+	format = SND_PCM_FORMAT_S16_LE;
+	channels = 2;
+	access_type = SND_PCM_ACCESS_RW_INTERLEAVED;
+	rate = 44100;
+	frames_to_read = 32;
+
+	DEVICE_OPENED = false;
+	PARAMS_SETED = false;
+	output_buffer_size = frames_to_read * snd_pcm_format_width(format) / 8 * 2;
+
+	output_buffer = (char *)malloc(output_buffer_size);
+}
+
+int voice_capture::open_device()
+{
+	if (!DEVICE_OPENED)
+	{
+
+		if ((err = snd_pcm_open(&handle, device, SND_PCM_STREAM_CAPTURE, 0)) < 0)
+		{
+			LOG(ERROR) << "Capture device open error.";
+			DEVICE_OPENED = false;
+			return -1;
+		}
+		else
+		{
+			LOG(INFO) << "Device \"" << device << "\" open success.";
+			DEVICE_OPENED = true;
+		}
+	}
+	return 0;
+}
+
+int voice_capture::set_params()
+{
+	if (!PARAMS_SETED)
+	{
+		snd_pcm_hw_params_alloca(&params);
+
+		if ((err = snd_pcm_hw_params_any(handle, params)) < 0)
+		{
+			LOG(ERROR) << "Initialize parameters error.";
+			return -1;
+		}
+		else
+		{
+			LOG(INFO) << "Initialize parameters success.";
+		}
+
+		if ((err = snd_pcm_hw_params_set_access(handle, params, access_type)) < 0)
+		{
+			LOG(ERROR) << "Set access type error.";
+			return -1;
+		}
+		else
+		{
+			LOG(INFO) << "Set access type success.";
+		}
+
+		if ((err = snd_pcm_hw_params_set_format(handle, params, format)) < 0)
+		{
+			LOG(ERROR) << "Set format error.";
+			return -1;
+		}
+		else
+		{
+			LOG(INFO) << "Set format success.";
+		}
+
+		channels = 2;
+		if ((err = snd_pcm_hw_params_set_channels(handle, params, channels)) < 0)
+		{
+			LOG(ERROR) << "Set channels error.";
+			return -1;
+		}
+		else
+		{
+			LOG(INFO) << "Set channels success.";
+		}
+
+		unsigned int rrate;
+		rrate = rate;
+		if ((err = snd_pcm_hw_params_set_rate_near(handle, params, &rate, 0)) < 0)
+		{
+			LOG(ERROR) << "Set rate error.";
+			return -1;
+		}
+		else
+		{
+			LOG(INFO) << "Set rate to " << rate << "Hz success.";
+			if (rrate != rate)
+			{
+				LOG(WARN) << "Rate " << rrate << "Hz not available, get " << rate << "Hz.";
+			}
+		}
+
+		if ((err = snd_pcm_hw_params(handle, params)) < 0)
+		{
+			LOG(ERROR) << "Set parameters error.";
+			return -1;
+		}
+		else
+		{
+			LOG(INFO) << "Set parameters success.";
+			PARAMS_SETED = true;
+		}
+	}
+	return 0;
+}
+
+int voice_capture::capture()
+{
+	if (open_device() ||
+		set_params() < 0)
+		return -1;
+
+	if ((frames_readed = snd_pcm_readi(handle, output_buffer, frames_to_read)) < 0)
+	{
+		LOG(ERROR) << "Read frame error.";
+		return -1;
+	}
+	else
+	{
+		LOG(INFO) << "Read frames success.";
+		return frames_readed;
+	}
+}
+
+voice_capture::~voice_capture()
+{
+	if (DEVICE_OPENED)
+	{
+		if ((err = snd_pcm_close(handle)) < 0)
+		{
+			LOG(ERROR) << "Close Device error.";
+		}
+		else
+		{
+			LOG(INFO) << "Close Device success.";
+		}
+	}
+	free(output_buffer);
+	delete[] device;
+}
+}
+}
--- an-core/lib/voice_capture.h
+++ an-core/lib/voice_capture.h
@@ -1,14 +1,44 @@
 #ifndef _VOICE_CAPTURE_H_
 #define _VOICE_CAPTURE_H_
 
+#include <alsa/asoundlib.h>
+
 namespace an
 {
 namespace core
 {
-
 class voice_capture
 {
-}
+  public:
+	char *output_buffer;
+	int channels;
+	int frames_to_read;
+	unsigned int output_buffer_size;
+	unsigned int rate;
+
+	voice_capture();
+	voice_capture(const char *t_device);
+	voice_capture(voice_capture &&) = default;
+	voice_capture(const voice_capture &) = default;
+	voice_capture &operator=(voice_capture &&) = default;
+	voice_capture &operator=(const voice_capture &) = default;
+	~voice_capture();
+
+	int capture();
+
+  private:
+	char *device;
+	int err, frames_readed;
+	bool DEVICE_OPENED;
+	bool PARAMS_SETED;
+	snd_pcm_t *handle;
+	snd_pcm_hw_params_t *params;
+	snd_pcm_format_t format;
+	snd_pcm_access_t access_type;
+
+	int open_device();
+	int set_params();
+};
 }
 }
 
