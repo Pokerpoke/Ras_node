efb0790200f04c7fb7aac34bcfa1ebe0b6c57888
Jiang <j824544269@gmail.com>
2017-11-20 19:50:57 +0800

    sync

--- scripts/Tiny4412_copy_binary_and_library.sh
+++ scripts/Tiny4412_copy_binary_and_library.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-# cd ..
-# if [ -d ./build ] 
-# then
-#     rm -rf build
-# fi
-
-# mkdir build
-# cd build
+read -p "Enter destation IP: " DESTIP
 
 cd ../build/
 
@@ -18,8 +11,8 @@ fi
 
 mkdir Tiny4412
 cd Tiny4412
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TARGET=Tiny4412 ../../
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_TARGET=Tiny4412 ../../
 make
 
-scp -r ./bin/* root@192.168.0.22:/home/aero-node
-scp ./lib/* root@192.168.0.22:/usr/lib/
+scp -r ./bin/* root@$DESTIP:/home/aero-node
+scp ./lib/* root@$DESTIP:/usr/lib/
