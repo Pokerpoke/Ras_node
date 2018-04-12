2d560c96cbc774a17863b285b8c999a2d18d0ee1
Jiang Yang <j824544269@gmail.com>
2018-04-12 04:58:28 -0700

    Update set up environment Fix - update url of git repository to 192.168.0.9 - add libasound2-dev Style - change /bin/sh to /bin/bash

--- scripts/set-up-cross-compile-environment.sh
+++ scripts/set-up-cross-compile-environment.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ################################################################################
 # 
 # Copyright (c) 2018 南京航空航天大学 航空通信网络研究室
@@ -30,6 +30,7 @@ DEPENDENCIES=(
               "liblog4cpp5-dev"
               "qt4-dev-tools"
               "libqtwebkit-dev"
+              "libasound2-dev"
               )
 
 for DEP in ${DEPENDENCIES[@]} ; do
@@ -51,46 +52,22 @@ then
     rm -rf aero-node-tools
 fi
 
-# clone repository
-# github repository
-# git clone https://github.com/Pokerpoke/aero-node-tools.git
-# for a high speed, will use git repository of lab LAN instead
-git clone http://192.168.0.7:3000/pokerpoke/aero-node-tools.git
-# mkdir aero-node-tools && cd aero-node-tools
-# wget -c -t 5 https://raw.githubusercontent.com/Pokerpoke/aero-node-tools/master/arm-linux-gcc-4.5.1-v6-vfp-20120301.tgz
-# wget -c -t 5 https://raw.githubusercontent.com/Pokerpoke/aero-node-tools/master/target-qte-4.8.5-to-hostpc.tgz
-if [ $? -ne 0 ]
-then
-    echo "git clone failed, plaese try again"
-    exit 1
-fi
+mkdir ${CMAKE_SOURCE_DIR}/scripts/aero-node-tools && cd ${CMAKE_SOURCE_DIR}/scripts/aero-node-tools
+
+wget -c -t 5 http://192.168.0.9/share/aero-node-tools/log4cpp-1.1.3.tar.gz
+wget -c -t 5 http://192.168.0.9/share/aero-node-tools/target-qte-4.8.5-to-hostpc.tgz
+wget -c -t 5 http://192.168.0.9/share/aero-node-tools/arm-linux-gcc-4.5.1-v6-vfp-20120301.tgz
 
 # need root permitted
-cd aero-node-tools
 sudo tar xvzf ./arm-linux-gcc-4.5.1-v6-vfp-20120301.tgz -C /
 sudo tar xvzf ./target-qte-4.8.5-to-hostpc.tgz -C /
-
-# build log4cpp
-# use wget to download log4cpp.tar.gz
-# try for 5 times
-# however it goes wrong from time to time
-# wget -c -t 5 --secure-protocol=TLSv1 \
-#              https://sourceforge.net/projects/log4cpp/files/log4cpp-1.1.x%20%28new%29/log4cpp-1.1/log4cpp-1.1.3.tar.gz
-# if [ $? -ne 0 ]
-# then
-#     echo "log4cpp download failed, please try again"
-#     exit 1
-# fi
 
 # unpack
 tar xvzf log4cpp-1.1.3.tar.gz && cd log4cpp
 
-# if download delete it
-# if use git, will delete the project later
-# rm log4cpp-1.1.3.tar.gz
-
 # build and clean for host
 # IMPORTANT: it will confilct while use suao apt install liblog4cpp
+# 
 # ./configure && make && sudo make install && make clean
 
 # build for cross compile toolschain
@@ -102,7 +79,7 @@ make
 # it is unable to pass environment variable to sudo commands
 # so, use a shell script to run make install
 cat > ${CMAKE_SOURCE_DIR}/scripts/temp_make-install.sh << EOF
-#!/bin/sh
+#!/bin/bash
 
 export PATH=/opt/FriendlyARM/toolschain/4.5.1/bin:$PATH
 
@@ -166,9 +143,9 @@ function git_cmake()
     cd ${CMAKE_SOURCE_DIR}/scripts && rm -rf ${PROJECT_DIR}
 }
 
-git_cmake https://github.com/j0r1/JThread.git
-git_cmake https://github.com/j0r1/JRTPLIB.git
-git_cmake https://github.com/BelledonneCommunications/bcg729.git
+git_cmake http://192.168.0.9:8086/git/Pokerpoke/JThread.git
+git_cmake http://192.168.0.9:8086/git/Pokerpoke/JRTPLIB.git
+git_cmake http://192.168.0.9:8086/git/Pokerpoke/bcg729.git
 
 # update ldconfig
 sudo ldconfig
