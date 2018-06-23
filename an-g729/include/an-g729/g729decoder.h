f2bfd7bed9bf4e6bf655afdc6d0624d839c584d1
Jiang Yang <j824544269@gmail.com>
2018-06-22 20:37:22 -0700

    Use ustc instead of tuna

---
+++ an-g729/include/an-g729/g729decoder.h
@@ -0,0 +1,50 @@
+/**
+ * 
+ * Copyright (c) 2018 南京航空航天大学 航空通信网络研究室
+ * 
+ * @file
+ * @author   姜阳 (j824544269@gmail.com)
+ * @date     2018-01
+ * @brief    
+ * @version  0.0.1
+ * 
+ * Last Modified:  2018-04-16
+ * Modified By:    姜阳 (j824544269@gmail.com)
+ * 
+ */
+#ifndef _G729DECODER_H_
+#define _G729DECODER_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <bcg729/decoder.h>
+#ifdef __cplusplus
+}
+#endif
+
+namespace an
+{
+namespace codec
+{
+class G729Decoder
+{
+  public:
+    G729Decoder();
+    ~G729Decoder();
+
+    int decoder(const char *input_buffer,
+                const int input_buffer_size);
+
+    char *output_buffer;
+    int output_buffer_size;
+
+  protected:
+    bcg729DecoderChannelContextStruct *decoderChannelContext;
+
+  private:
+};
+}
+}
+
+#endif // !_G729DECODER_H_
