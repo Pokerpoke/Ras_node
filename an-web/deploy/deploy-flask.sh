68ee836541e4e84f6a673e7b77c7a11a04dd693d
Jiang Yang <j824544269@gmail.com>
2018-11-26 22:02:32 +0800

    update deploy-flask

--- an-web/deploy/deploy-flask.sh
+++ an-web/deploy/deploy-flask.sh
@@ -12,3 +12,24 @@
 # Modified By:    Jiang Yang (pokerpoke@qq.com)
 # 
 ################################################################################
+set -e
+
+# get scripts directory
+CMAKE_SOURCE_DIR=$(dirname $(readlink -f $0))
+cd ${CMAKE_SOURCE_DIR}/../..
+# update cmake source path
+CMAKE_SOURCE_DIR=$(pwd)
+cd ${CMAKE_SOURCE_DIR}/an-web
+
+if [ -d venv ] 
+then
+   rm -rf venv 
+fi
+
+mkdir venv && cd venv
+python3 -m venv .
+cd ..
+
+source venv/bin/activate
+
+pip3 install -r requirements.txt
